e5ef7dbe27bf959ac93c18a7d49a7b41d4ab3a01
sarbajitdutta <sarbajitdutta35@gmail.com>
2021-08-05 14:39:17 -0400

    add waf stack to main stack

--- src/main/java/edu/wcm/reciter/ReCiterCdkStack.java
+++ src/main/java/edu/wcm/reciter/ReCiterCdkStack.java
@@ -2,6 +2,8 @@ package edu.wcm.reciter;
 
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.NestedStack;
+import software.amazon.awscdk.core.NestedStackProps;
+import software.amazon.awscdk.core.RemovalPolicy;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
 
@@ -13,10 +15,30 @@ public class ReCiterCdkStack extends Stack {
     public ReCiterCdkStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        ReCiterCDKVPCStack reCiterCDKVPCStack = new ReCiterCDKVPCStack(this, "reCiterCDKVPCStack");
+        ReCiterCDKVPCStack reCiterCDKVPCStack = new ReCiterCDKVPCStack(this, "reCiterCDKVPCStack", NestedStackProps.builder()
+            .removalPolicy(RemovalPolicy.DESTROY)
+            .build());
 
-        ReCiterCDKECRStack reCiterCDKECRStack = new ReCiterCDKECRStack(this, "reCiterCDKECRStack");
+        ReCiterCDKECRStack reCiterCDKECRStack = new ReCiterCDKECRStack(this, "reCiterCDKECRStack", NestedStackProps.builder()
+            .removalPolicy(RemovalPolicy.DESTROY)
+            .build());
         NestedStack.isNestedStack(reCiterCDKECRStack);
         reCiterCDKECRStack.addDependency(reCiterCDKVPCStack, "This is dependent on VPC Resources being created");
+
+        ReCiterCDKECSStack reCiterCDKECSStack = new ReCiterCDKECSStack(this, "reCiterCDKECSStack", NestedStackProps.builder()
+            .removalPolicy(RemovalPolicy.DESTROY)
+            .build(),
+            reCiterCDKVPCStack.getVpc());
+        NestedStack.isNestedStack(reCiterCDKECSStack);
+        reCiterCDKECSStack.addDependency(reCiterCDKVPCStack, "ECS Stack is dependent on VPC");
+        reCiterCDKECSStack.addDependency(reCiterCDKECRStack, "ECR repo is needed for ECS Tasks");
+
+        ReCiterCdkWAFStack reCiterCdkWAFStack = new ReCiterCdkWAFStack(this, "reCiterCdkWAFStack", NestedStackProps.builder()
+            .removalPolicy(RemovalPolicy.DESTROY)
+            .build(), 
+            reCiterCDKECSStack.getAlb());
+        NestedStack.isNestedStack(reCiterCdkWAFStack);
+        reCiterCdkWAFStack.addDependency(reCiterCDKECSStack, "WAF dependent on ALB");
+        
     }
 }
