11b7334d117ef35f342d030a0038a1189faf97fe
sarbajitdutta <sarbajitdutta35@gmail.com>
2021-08-11 14:11:04 -0400

    add test cases

--- src/test/java/edu/wcm/reciter/ReCiterCdkTest.java
+++ src/test/java/edu/wcm/reciter/ReCiterCdkTest.java
@@ -1,17 +1,18 @@
 package edu.wcm.reciter;
 
-import software.amazon.awscdk.core.App;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
-import java.io.IOException;
-
 import org.junit.jupiter.api.Test;
 
-import edu.wcm.reciter.ReCiterCdkStack;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import software.amazon.awscdk.core.App;
+import software.amazon.awscdk.core.StackProps;
 
 public class ReCiterCdkTest {
     private final static ObjectMapper JSON =
@@ -20,12 +21,45 @@ public class ReCiterCdkTest {
     @Test
     public void testStack() throws IOException {
         App app = new App();
-        ReCiterCdkStack stack = new ReCiterCdkStack(app, "test");
-
-        // synthesize the stack to a CloudFormation template
+        ReCiterCdkStack stack = new ReCiterCdkStack(app, "masterStack", 
+        StackProps.builder()
+        //.env(ReCiterCdkApp.makeEnv(System.getenv("CDK_DEFAULT_ACCOUNT"), System.getenv("CDK_DEFAULT_REGION")))
+        .build());
+        // synthesize the stack to a CloudFormation     template
         JsonNode actual = JSON.valueToTree(app.synth().getStackArtifact(stack.getArtifactId()).getTemplate());
 
         // Update once resources have been added to the stack
-        //assertThat(actual.get("Resources")).isNull();
+        assertThat(actual.toString().contains("AWS::CloudFormation::Stack"));
+        assertThat(actual.toString().contains("AWS::Logs::LogGroup"));
+    }
+
+    @Test
+    public void testEnvVariables() throws IOException {
+        String adminApikey = System.getenv("ADMIN_API_KEY");
+        assertNotNull(adminApikey, "ADMIN_API_KEY environment variable is required for stack to run");
+
+        String consumerApiKey = System.getenv("CONSUMER_API_KEY");
+        assertNotNull(consumerApiKey, "CONSUMER_API_KEY environment variable is required for stack to run");
+
+        String pubmedApiKey = System.getenv("PUBMED_API_KEY");
+        assertNotNull(pubmedApiKey, "PUBMED_API_KEY environment variable is required for stack to run");
+
+        String includeScopus = System.getenv("INCLUDE_SCOPUS");
+        assertNotNull(includeScopus, "INCLUDE_SCOPUS environment variable is required for stack to run");
+
+        String alarmEmail = System.getenv("ALARM_EMAIL");
+        assertNotNull(alarmEmail, "ALARM_EMAIL environment variable is required for stack to run");
+
+        String githubUser = System.getenv("GITHUB_USER");
+        assertNotNull(githubUser, "GITHUB_USER environment variable is required for stack to run");
+
+        String githubPersonalAccessToken = System.getenv("GITHUB_PERSONAL_ACCESS_TOKEN");
+        assertNotNull(githubPersonalAccessToken, "GITHUB_PERSONAL_ACCESS_TOKEN environment variable is required for stack to run");
+
+        String scopusApiKey = System.getenv("SCOPUS_API_KEY");
+        assertNotNull(scopusApiKey, "SCOPUS_API_KEY environment variable is required for stack to run");
+
+        String scopusInstToken = System.getenv("SCOPUS_INST_TOKEN");
+        assertNotNull(scopusInstToken, "SCOPUS_INST_TOKEN environment variable is required for stack to run");
     }
 }
